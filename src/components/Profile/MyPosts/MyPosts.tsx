80a354c3b0678d0ed16ff2519b6c177ef8eda42e
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-05-12 12:28:58 +0300

    27 add render

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,15 +3,18 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post'
 
 
-const MyPosts = () => {
-    let posts = [
-        {id: 1, message: 'Hi,how are you', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 11},
-        {id: 3, message: 'blabla', likesCount: 11},
-        {id: 4, message: 'dada', likesCount: 11},
-    ]
+type myPostProps = {
+    posts: postType[]
+}
+type postType = {
+    id: number
+    message: string
+    likesCount: number
+}
+const MyPosts = (props: myPostProps) => {
 
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={s.postsBlock}>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,12 +3,19 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 
+let posts = [
+    {id: 1, message: 'Hi,how are you', likesCount: 12},
+    {id: 2, message: 'It\'s my first post', likesCount: 11},
+    {id: 3, message: 'blabla', likesCount: 11},
+    {id: 4, message: 'dada', likesCount: 11},
+]
+
 
 const Profile = () => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </div>
     )
 }
