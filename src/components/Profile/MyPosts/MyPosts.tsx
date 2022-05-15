3e1b1e081cc7176fdd7233cfad178d2576f88064
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-05-15 15:48:05 +0300

    31 знакомство с useref

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post'
 import {postType} from '../../../redux/state';
@@ -11,15 +11,25 @@ const MyPosts = (props: myPostProps) => {
 
     let postsElements = props.posts.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
 
+    let newPostEl = useRef<HTMLTextAreaElement>(null)
+
+    const addPost = () => {
+        if (newPostEl.current !== null) {
+            alert(newPostEl.current.value)
+        }
+    }
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostEl}></textarea>
                 </div>
                 <div>
-                    <button>add post</button>
+                    <button onClick={addPost}>
+                        add post
+                    </button>
                 </div>
             </div>
             <div className={s.posts}>
